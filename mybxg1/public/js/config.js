71f4e2cc068147f7d79febb6fe25c23b0982b2cb
guo7687907 <450282193@qq.com>
2017-09-30 20:55:26 +0800

    表单提交和验证插件

--- mybxg1/public/js/config.js
+++ mybxg1/public/js/config.js
@@ -5,6 +5,10 @@ require.config({
     cookie:'jquery-cookie/jquery.cookie',
     template:'artTemplate/template-web',
     bootstrap:'bootstrap/js/bootstrap.min',
+    datepicker:'bootstrap-datepicker/js/bootstrap-datepicker',
+    language:'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
+    validate:'validate/jquery-validate.min',
+    form : 'jquery-form/jquery.form',
     util:'../js/util',
     common:'../js/common',
     login :'../js/login',
@@ -12,6 +16,14 @@ require.config({
     teacheradd:'../js/teacher-add'
   },
   shim:{
-    deps : ['jquery']
+    bootstrap: {
+      deps: ['jquery']
+    },
+    language:{
+      deps:['jquery','datepicker']
+    },
+    validate:{
+      deps:['jquery']
+    }
   }
 });
--- mybxg1/public/js/teacher-add.js
+++ mybxg1/public/js/teacher-add.js
@@ -1,45 +1,79 @@
-define(['jquery', 'template','util'], function ($, template,util) {
+define(['jquery', 'template', 'util', 'datepicker', 'language', 'validate', 'form'], function ($, template, util) {
   var tcId = util.qs('tc_id')
   console.log(tcId)
-  if(tcId){
+  if (tcId) {
     //编辑操作
     $.ajax({
-      type:'get',
-      url:'/api/teacher/edit',
-      data:{
-        tc_id:tcId
+      type: 'get',
+      url: '/api/teacher/edit',
+      data: {
+        tc_id: tcId
       },
-      dataType:'json',
+      dataType: 'json',
       success: function (data) {
         console.log(data)
         data.result.operate = '编辑讲师';
-        var html = template('teacherTpl',data.result);
+        var html = template('teacherTpl', data.result);
         $('#teacherInfo').html(html);
         submitForm('/api/teacher/update');
       }
 
     })
-  }else{
+  } else {
     //添加操作
-    var html = template('teacherTpl',{operate:'添加讲师'});
+    var html = template('teacherTpl', {operate: '添加讲师'});
     $('#teacherInfo').html(html);
     submitForm('/api/teacher/add');
   }
 
-  //提交表单函数
-  function submitForm(url){
-    $('#teacherBtn').click(function () {
-      $.ajax({
-        type:'post',
-        url:url,
-        data:$('#teacherForm').serialize(),
-        dataType:'json',
-        success: function (data) {
-          if(data.code == 200){
-            location.href = '/teacher/list';
+  //插件实现表单提交功能
+  function submitForm(url) {
+    $("#teacherForm").validate({
+      //阻止跳转
+      sendForm: false,
+      valid: function () {
+        console.log('success')
+        $(this).ajaxSubmit({
+          url: url,
+          dataType: 'json',
+          success: function (data) {
+            if (data.code == 200) {
+              location.href = '/teacher/list';
+            }
           }
+        })
+      },
+      description: {
+        tcName: {
+          required: '用户名不能为空'
+        },
+        tcPass: {
+          required: '密码不能为空',
+          pattern: '必须为六位数字'
+        },
+        tcJoinDate: {
+          required: '日期不能为空'
         }
-      })
+      }
+
     })
-  };
+  }
+
+
+//  //提交表单函数
+//  function submitForm(url){
+//    $('#teacherBtn').click(function () {
+//      $.ajax({
+//        type:'post',
+//        url:url,
+//        data:$('#teacherForm').serialize(),
+//        dataType:'json',
+//        success: function (data) {
+//          if(data.code == 200){
+//            location.href = '/teacher/list';
+//          }
+//        }
+//      })
+//    })
+//  };
 });
