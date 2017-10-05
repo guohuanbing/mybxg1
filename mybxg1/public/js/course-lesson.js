6d1f3c3d605b5fef5e2d2bc9bf4c953410103328
guo7687907 <450282193@qq.com>
2017-10-05 23:42:46 +0800

    添加和编辑课时表单提交

--- mybxg1/public/js/course-lesson.js
+++ mybxg1/public/js/course-lesson.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','bootstrap'], function ($, template, util) {
+define(['jquery', 'template', 'util', 'bootstrap', 'form'], function ($, template, util) {
   //设置导航菜单
   util.setMenu('/course/add');
 
@@ -6,41 +6,73 @@ define(['jquery','template','util','bootstrap'], function ($, template, util) {
   var csId = util.qs('cs_id');
 
   $.ajax({
-    type:'get',
-    url:'/api/course/lesson',
-    data:{cs_id:csId},
-    dataType:'json',
+    type: 'get',
+    url: '/api/course/lesson',
+    data: {cs_id: csId},
+    dataType: 'json',
     success: function (data) {
       console.log(data)
-      var html = template('lessonTpl',data.result);
+      var html = template('lessonTpl', data.result);
       $('#lessonInfo').html(html);
 
       //处理添加课时
       $("#addLesson").click(function () {
-        var html = template('modalTpl',{operate:'添加课时'})
+        var html = template('modalTpl', {operate: '添加课时'})
         $('#modalInfo').html(html);
         $("#chapterModal").modal();
-      })
 
+        //处理表单提交
+        $('#addOreditBtn').click(function () {
+          $('#lessonForm').ajaxSubmit({
+            type: 'post',
+            url: '/api/course/chapter/add',
+            dataType: 'json',
+            data: {ct_cs_id: csId},
+            success: function (data) {
+              //console.log(data)
+              if (data.code == 200) {
+                location.reload();
+              }
+            }
+          });
+        })
+      })
 
       //处理课时编辑
       $(".editLesson").click(function () {
         //获取课时ID
         var ctId = $(this).attr('data-ctId')
         $.ajax({
-          type:'get',
-          url:'/api/course/chapter/edit',
-          data:{ct_id:ctId},
-          dataType:'json',
+          type: 'get',
+          url: '/api/course/chapter/edit',
+          data: {ct_id: ctId},
+          dataType: 'json',
           success: function (data) {
             console.log(data)
             data.result.operate = '编辑课时';
-            var html = template('modalTpl',data.result)
+            var html = template('modalTpl', data.result)
             $('#modalInfo').html(html);
+            $("#chapterModal").modal();
+
+            //处理表单提交
+            $('#addOreditBtn').click(function () {
+              $('#lessonForm').ajaxSubmit({
+                type: 'post',
+                url: '/api/course/chapter/modify',
+                dataType: 'json',
+                data: {ct_cs_id: csId,ct_id:ctId},
+                success: function (data) {
+                  //console.log(data)
+                  if (data.code == 200) {
+                    location.reload();
+                  }
+                }
+              });
+            });
           }
         })
-        $("#chapterModal").modal();
       })
+
     }
   });
 })
