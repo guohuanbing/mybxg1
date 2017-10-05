54ad5d768f3eb376e95cc1b3e5e871b7ddd0a9e6
guo7687907 <450282193@qq.com>
2017-10-05 10:44:33 +0800

    课程基本信息填充

--- mybxg1/public/js/course-basic.js
+++ mybxg1/public/js/course-basic.js
@@ -4,7 +4,7 @@ define(['jquery','template','util'], function ($,template,util) {
   var csId = util.qs('cs_id');
   //找到地址栏中的标记
   var flag = util.qs('flag');
-  console.log(csId)
+  //console.log(csId)
   $.ajax({
     type:'get',
     url:'/api/course/basic',
@@ -18,9 +18,25 @@ define(['jquery','template','util'], function ($,template,util) {
         //如果没有，是 添加课程
         data.result.operate = '添加课程'
       }
-      console.log(data)
+      //console.log(data)
       var html = template('basicTpl',data.result)
       $("#basicInfo").html(html)
+      $("#firstType").change(function () {
+        var pid = $(this).val();
+        $.ajax({
+          type:'get',
+          url:'/api/category/child',
+          data:{cg_id:pid},
+          dataType:'json',
+          success: function (data) {
+            console.log(data)
+            var tpl = '<select name="" class="form-control input-sm"><option value="">请选择二级分类...</option>{{each list}} <option value="{{$value.cg_id}}">{{$value.cg_name}}</option>{{/each}} </select>';
+            var html = template.render(tpl,{list:data.result});
+            console.log(html)
+            $("#secondType").html(html)
+          }
+        })
+      })
     }
   })
 })
