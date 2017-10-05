1764f8f0852ff7611f59712c7568ffcf850e7393
guo7687907 <450282193@qq.com>
2017-10-05 11:24:23 +0800

    完成课程基本信息

--- mybxg1/public/js/course-basic.js
+++ mybxg1/public/js/course-basic.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'], function ($,template,util) {
+define(['jquery','template','util','validate','form'], function ($,template,util) {
   util.setMenu('/course/add');
   //获取地址栏中的课程ID
   var csId = util.qs('cs_id');
@@ -36,6 +36,23 @@ define(['jquery','template','util'], function ($,template,util) {
             $("#secondType").html(html)
           }
         })
+      })
+
+      //处理表单提交
+      $("#basicForm").validate({
+        sendForm:false,
+        valid: function () {
+          $(this).ajaxSubmit({
+            type:'post',
+            url:'/api/course/update/basic',
+            data:{cs_id:csId},
+            dataType:'json',
+            success: function (data) {
+              console.log(data)
+              location.href = '/course/picture?cs_id='+data.result.cs_id;
+            }
+          })
+        }
       })
     }
   })
---
+++ mybxg1/public/js/course-picture.js
@@ -0,0 +1,5 @@
+define(['jquery','template','util'], function ($,template,util) {
+  //设置导航菜单
+  util.setMenu('/course/add');
+  
+})
